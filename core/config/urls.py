ebdc41e4a320bcd22abac72bfec9a19f95dc59e5
Reinier Hernández <sasuke.reinier@gmail.com>
2021-09-21 14:12:33 -0400

    agregando urls a la aplicacion accounts

--- core/config/urls.py
+++ core/config/urls.py
@@ -7,4 +7,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include('justshipto_core.core.urls')),
+    path('accounts/', include('justshipto_core.accounts.urls')),
 ]
