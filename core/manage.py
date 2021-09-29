2039eeba65c1dde1a8a93d484dddc6b1c49079a5
Carlos Lugones <contact@lugodev.com>
2021-09-29 15:38:57 -0400

    perf: manage.py envs

--- core/manage.py
+++ core/manage.py
@@ -2,11 +2,23 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.develop')
+
+    if os.getenv('ENV') == 'develop':
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.develop')
+    
+    elif os.getenv('ENV') == 'staging':
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.staging')
+    
+    elif os.getenv('ENV') == 'production':
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +27,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
     execute_from_command_line(sys.argv)
 
 
