4e68bf6f9a4d24fabc346864a5284805da447ab8
Carlos Lugones <contact@lugodev.com>
2021-09-29 16:17:24 -0400

    feat: Mail model

--- core/justshipto_core/mails/models.py
+++ core/justshipto_core/mails/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from justshipto_core.core.models import TimeStampedModel
+
+
+class EmailLog(TimeStampedModel):
+    sender = models.EmailField()
+    to = models.EmailField()
+    subject = models.CharField(max_length=255)
+    html = models.TextField()
+
+    def __str__(self):
+        return str(self.id)
