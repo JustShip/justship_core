0ee44446701cfcd1100cebe75173056a836f14c7
Carlos Lugones <contact@lugodev.com>
2021-09-29 16:21:16 -0400

    feat: EmailLogAdmin

--- core/justshipto_core/mails/admin.py
+++ core/justshipto_core/mails/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.EmailLog)
+class EmailLogAdmin(admin.ModelAdmin):
+    fields = ('sender', 'to', 'subject', 'html')
+    list_display = ('sender', 'to', 'subject', 'created_at', '_get_html')
+    readonly_fields = ('sender', 'to', 'subject', 'html', 'created_at', 'updated_at')
+    search_fields = ('email',)
+
+    def _get_html(self, obj):
+        return mark_safe(obj.html)
