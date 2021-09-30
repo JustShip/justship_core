370cf8f67d62d6c1277095ee30ca2779709c4bf4
Reinier Hernández <sasuke.reinier@gmail.com>
2021-09-30 09:49:28 -0400

    fix: obteniendo el usuario a modificar del request

--- justship/apps/accounts/api/mutations.py
+++ justship/apps/accounts/api/mutations.py
@@ -6,16 +6,14 @@ from justship.apps.accounts.models import User
 
 class UpdateUsername(graphene.Mutation):
     class Arguments:
-        identifier = graphene.Int()
         username = graphene.String()
     ok = graphene.Boolean()
     user = graphene.Field(UserType)
 
     @staticmethod
-    def mutate(root, info, identifier, username):
-        current_user = info.context.user
-        user = User.objects.get(pk=identifier)
-        if user and (user == current_user or current_user.is_staff):
+    def mutate(root, info, username):
+        user = info.context.user
+        if not user.is_anonymous:
             user.username = username if username is not None else user.username
             user.save()
             return UpdateUsername(ok=True, user=user)
