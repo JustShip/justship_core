c4281cc4c202bbc588f959a63e6f6be1e7cfacf5
Asiel Cabrera <cabrerasiel@gmail.com>
2021-09-29 22:34:44 -0400

    UDATE: poetry script celery

--- scripts.py
+++ scripts.py
@@ -1,17 +1,16 @@
-from __future__ import absolute_import, unicode_literals
 import subprocess
 import threading
 
-  
 
 def django():
     cmd = ["python", "justship/manage.py", "runserver", "0.0.0.0:8000"]
     subprocess.run(cmd)
 
+
 def celery():
-    cmd = ["celery", "-A justship.config", "worker -l INFO"]
+    cmd = ["/bin/zsh", "celery.sh"]
     subprocess.run(cmd)
-    
+
 
 def server():
     t1 = threading.Thread(target=celery)
