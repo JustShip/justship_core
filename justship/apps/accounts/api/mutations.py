5d732bef2c4cde16c6068ee011e935b2df501493
Reinier Hernández <sasuke.reinier@gmail.com>
2021-09-30 09:16:01 -0400

    feat: update username

--- justship/apps/accounts/api/mutations.py
+++ justship/apps/accounts/api/mutations.py
@@ -1,5 +1,25 @@
 import graphene
 
+from justship.apps.accounts.api.types import UserType
+from justship.apps.accounts.models import User
 
-class UserMutations:
-    pass
+
+class UpdateUsername(graphene.Mutation):
+    class Arguments:
+        identifier = graphene.Int()
+        username = graphene.String()
+    ok = graphene.Boolean()
+    user = graphene.Field(UserType)
+
+    @staticmethod
+    def mutate(root, info, identifier, username):
+        user = User.objects.get(pk=identifier)
+        if user:
+            user.username = username if username is not None else user.username
+            user.save()
+            return UpdateUsername(ok=True, user=user)
+        return UpdateUsername(ok=False, user=None)
+
+
+class UserMutations(graphene.ObjectType):
+    update_username = UpdateUsername.Field()
