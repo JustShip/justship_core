28963a067bc3249c4a0cc18aa55f34917d04b670
Reinier Hernández <sasuke.reinier@gmail.com>
2021-09-30 09:42:28 -0400

    fix: el username solo lo puede cambiar el mismo usuario o miembro del staff

--- justship/apps/accounts/api/mutations.py
+++ justship/apps/accounts/api/mutations.py
@@ -13,8 +13,9 @@ class UpdateUsername(graphene.Mutation):
 
     @staticmethod
     def mutate(root, info, identifier, username):
+        current_user = info.context.user
         user = User.objects.get(pk=identifier)
-        if user:
+        if user and (user == current_user or current_user.is_staff):
             user.username = username if username is not None else user.username
             user.save()
             return UpdateUsername(ok=True, user=user)
