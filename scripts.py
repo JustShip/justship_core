0c543c62ad9fe1b9f50d663ecd0e95158424d14f
Reinier Hernández <sasuke.reinier@gmail.com>
2021-09-30 08:16:28 -0400

    style: agregando metodo encargado de ejecutar los subprocesos

--- scripts.py
+++ scripts.py
@@ -1,19 +1,41 @@
 import subprocess
 import threading
+import shlex
 
 
 def django():
-    cmd = ["python", "justship/manage.py", "runserver", "0.0.0.0:8000"]
-    subprocess.run(cmd)
+    cmd = "python justship/manage.py runserver 0.0.0.0:8000"
+    __run_command(cmd)
+
 
 def create_super_user():
-    cmd = ["python", "justship/manage.py", "createsuperuser"]
-    subprocess.run(cmd)
+    cmd = "python justship/manage.py createsuperuser"
+    __run_command(cmd)
+
 
 def celery():
-    cmd = ["/bin/zsh", "celery.sh"]
-    subprocess.run(cmd)
+    cmd = "/bin/zsh celery.sh"
+    __run_command(cmd)
 
+
+def test() -> None:
+    """
+    Run django tests
+    :return: None
+    """
+    cmd = "python justship/manage.py test"
+    __run_command(cmd)
+
+
+def __run_command(command: str) -> str:
+    """
+    Run a command process
+    :param command: a string that represent the command to execute
+    :return: string with de stdout
+    """
+    parsed_command = shlex.split(command)
+    process = subprocess.run(parsed_command, stdout=subprocess.PIPE, universal_newlines=True)
+    return process.stdout
 
 
 def server():
