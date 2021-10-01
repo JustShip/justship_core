4e049cd93163de12fc2cd43db40216f7c917c0b1
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-01 16:44:15 -0400

    feat: agregando campo tipo de creador en el modelo user

--- justship/apps/accounts/admin.py
+++ justship/apps/accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
 from . import models
 
@@ -9,3 +10,13 @@ class UserAdmin(UserAdmin):
     list_display = ['username', 'get_full_name', 'is_superuser']
     list_filter = ['is_superuser']
     search_fields = ['username', 'first_name', 'last_name']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Otros datos', {'fields': ('creator_type',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Otros datos', {'fields': ('creator_type',)}),
+    )
+
+
+# Remove Group from Django admin
+admin.site.unregister(Group)
--- justship/apps/accounts/models.py
+++ justship/apps/accounts/models.py
@@ -1,6 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from . import constants
 
 
 class User(AbstractUser):
+    creator_type = models.CharField(
+        verbose_name='Tipo de Creador',
+        max_length=10,
+        choices=constants.CREATOR_TYPE_CHOICES
+    )
+
     def __str__(self):
         return self.get_full_name() or self.username
