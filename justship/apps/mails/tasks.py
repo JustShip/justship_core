8b7fc797563b922f56f7cbfe6671bd5749f3c952
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-01 10:45:47 -0400

    feat: agregando tarea de enviar mensajes de recuperacion de cuenta

--- justship/apps/mails/tasks.py
+++ justship/apps/mails/tasks.py
@@ -8,14 +8,14 @@ from .models import EmailLog
 EMAIL = settings.EMAIL_HOST_USER
 
 
-def render(template, context):
+def render(template: str, context: dict) -> str:
     with open('justship/apps/mails/templates/mails/' + template, 'r') as file:
         text = file.read()
         tmpl = Template(text)
     return tmpl.render(Context(context))
 
 
-def send_mail(sender, to, subject, html):
+def send_mail(sender: str, to: str, subject: str, html: str) -> None:
     msg = EmailMultiAlternatives(
         subject=subject,
         from_email=sender,
@@ -31,3 +31,21 @@ def send_mail(sender, to, subject, html):
         subject=subject,
         html=html
     )
+
+
+def send_system_mail(to: str, subject: str, html: str) -> None:
+    """
+    Send an email with from the email system
+    :param to: mail to send
+    :param subject: the subject of the email
+    :param html: html render with email
+    :return: None
+    """
+    send_mail(EMAIL, to, subject, html)
+
+
+@shared_task
+def send_recovery_mail(to: str) -> None:
+    context = {'hash': 1234}
+    template = render('recovery_mail.html', context)
+    send_system_mail(to, 'Recuperar contraseña', template)
