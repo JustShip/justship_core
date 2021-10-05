7dcf169bfd6331fb79a14622a1a61300527dfe28
Carlos Lugones <contact@lugodev.com>
2021-10-05 18:06:14 -0400

    feat: billing admin #22

--- justship/apps/billing/admin.py
+++ justship/apps/billing/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+@admin.register(models.BillingConfig)
+class BillingConfigAdmin(admin.ModelAdmin):
+    list_field = ('price_monthly', 'price_yearly')
+
+
+@admin.register(models.Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_field = ('user', 'number', 'amount', 'currency', 'state')
