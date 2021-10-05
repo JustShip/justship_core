f82477c17df7376ade1c999e15e650965e50af75
Carlos Lugones <contact@lugodev.com>
2021-10-05 17:57:38 -0400

    feat: Transaction model #22

--- justship/apps/billing/models.py
+++ justship/apps/billing/models.py
@@ -1,6 +1,9 @@
+import hashlib
 from django.db import models
+from django.utils.timezone import now
 
-from justship.apps.core.models import SingletonModel
+from justship.apps.core.models import SingletonModel, TimeStampedModel
+from justship.apps.accounts.models import User
 
 
 class BillingConfig(SingletonModel):
@@ -10,3 +13,35 @@ class BillingConfig(SingletonModel):
 
     def __str__(self):
         return 'Billing config'
+
+
+class Transaction(TimeStampedModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    number = models.CharField(max_length=255)
+    state = models.CharField(max_length=255, default='pending', choices=(
+        ('pending', 'Pending'),
+        ('confirmed', 'Confirmed'),
+        ('cancelled', 'Cancelled')
+    ))
+
+    # billing
+    amount = models.FloatField(default=1)
+    currency = models.CharField(max_length=3, default='USD')
+    payment_method = models.CharField(max_length=20, choices=(
+        ('paypal', 'PayPal'),
+        ('qvapay', 'QvaPay')
+    ))
+    days = models.IntegerField(default=30)
+
+    # QvaPay related
+    qvapay_id = models.CharField(max_length=255, null=True, blank=True)
+    qvapay_url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.number
+
+    def _do_insert(self, manager, using, fields, update_pk, raw):
+        self.number = hashlib.sha256(
+            str(str(now()) + str(self.id)).encode()
+        ).hexdigest()[:10]
+        return super()._do_insert(manager, using, fields, update_pk, raw)
