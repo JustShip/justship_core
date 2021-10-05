89ac3ec1d522ab71413d2e6c6ed735da34e9228d
Carlos Lugones <contact@lugodev.com>
2021-10-05 17:45:08 -0400

    feat: billing config #22

--- justship/apps/billing/models.py
+++ justship/apps/billing/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from justship.apps.core.models import SingletonModel
+
+
+class BillingConfig(SingletonModel):
+    price_monthly = models.FloatField()
+    price_yearly = models.FloatField()
+    currency = models.CharField(max_length=3, default='USD')
+
+    def __str__(self):
+        return 'Billing config'
