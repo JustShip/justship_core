2b3fd220e5f1897a19863400dabad3f19110eabd
Carlos Lugones <contact@lugodev.com>
2021-10-05 19:15:37 -0400

    feat: billing API #22

---
+++ justship/apps/billing/api/mutations.py
@@ -0,0 +1,5 @@
+import graphene
+
+
+class BillingMutations(graphene.ObjectType):
+    pass
---
+++ justship/apps/billing/api/schema.py
@@ -0,0 +1,3 @@
+from .mutations import BillingMutations
+from .queries import BillingQueries
+from .types import billing_types
