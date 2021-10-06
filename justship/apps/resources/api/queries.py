897b16faf1fab63ab8695397aa224b2ccee5120d
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-06 14:50:21 -0400

    feat: get one and all categories

--- justship/apps/resources/api/queries.py
+++ justship/apps/resources/api/queries.py
@@ -1,5 +1,15 @@
 import graphene
 
+from .types import CategoryType
+from .. import models
+
 
 class ResourceQueries:
-    pass
+    category = graphene.Field(CategoryType, category_id=graphene.Int())
+    categories = graphene.List(CategoryType)
+
+    def resolve_category(self, info, category_id):
+        return models.Category.objects.get(pk=category_id)
+
+    def resolve_categories(self, info):
+        return models.Category.objects.all()
