201557083525257a0ae97127248de520b9d307b0
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-06 11:58:32 -0400

    feat: agregando modelo category y asociandolo a resource

--- justship/apps/resources/models.py
+++ justship/apps/resources/models.py
@@ -1,10 +1,24 @@
 from django.conf import settings
 from django.db import models
 
+from justship.apps.core.models import TimeStampedModel
 
-class Resource(models.Model):
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=20)
+    description = models.CharField(verbose_name='Description', max_length=200, null=True, blank=True)
+
+
+class Resource(TimeStampedModel):
     url = models.URLField()
-    category = models.CharField(verbose_name='Category', max_length=100, null=True, blank=True)
+    category = models.ManyToManyField(
+        Category,
+        related_name='categories',
+        verbose_name='Category',
+        max_length=100,
+        null=True,
+        blank=True
+    )
     image = models.URLField(verbose_name='Image', null=True, blank=True)
     description = models.TextField(verbose_name='Description', null=True, blank=True)
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
