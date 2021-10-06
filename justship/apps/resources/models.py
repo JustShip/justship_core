b9e9b75711dff3061b3311acaa441ec92007617b
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-06 11:35:54 -0400

    fix: category y description pueden ser null en el modelo Resource

--- justship/apps/resources/models.py
+++ justship/apps/resources/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Resource(models.Model):
     url = models.URLField()
-    category = models.CharField(verbose_name='Category', max_length=100)
+    category = models.CharField(verbose_name='Category', max_length=100, null=True, blank=True)
     image = models.URLField(verbose_name='Image', null=True, blank=True)
-    description = models.TextField(verbose_name='Description')
+    description = models.TextField(verbose_name='Description', null=True, blank=True)
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
