470f60fbae38eb7d433c1b7e09d407e53575db5b
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-06 10:17:07 -0400

    fix: retornando el usuario sin importar las mayusculas

--- justship/apps/accounts/api/queries.py
+++ justship/apps/accounts/api/queries.py
@@ -7,13 +7,19 @@ from .types import UserType
 
 class UserQueries:
     me = graphene.Field(UserType)
-    profile = graphene.Field(UserType, id=graphene.Int())
+    profile = graphene.Field(UserType, username=graphene.String())
 
     def resolve_me(self, info, **kwargs):
         return info.context.user
 
-    def resolve_profile(self, info, **kwargs):
+    def resolve_profile(self, info, username):
+        """
+        Given the username, returns the user. If this isn't exists throw error
+        :param info: request and metadata info
+        :param username: username to search
+        :return: if user exists returns, else throw error
+        """
         try:
-            return get_user_model().objects.get(**kwargs)
+            return get_user_model().objects.get(username__iexact=username)
         except get_user_model().DoesNotExist:
             return GraphQLError('User does not exists')
