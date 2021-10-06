7c2b977df9ce572bfdfe11693c9f4f9e7538b81d
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-06 10:11:10 -0400

    feat: view profile

--- justship/apps/accounts/api/queries.py
+++ justship/apps/accounts/api/queries.py
@@ -1,10 +1,19 @@
 import graphene
+from django.contrib.auth import get_user_model
+from graphql import GraphQLError
 
 from .types import UserType
 
 
 class UserQueries:
     me = graphene.Field(UserType)
+    profile = graphene.Field(UserType, id=graphene.Int())
 
     def resolve_me(self, info, **kwargs):
         return info.context.user
+
+    def resolve_profile(self, info, **kwargs):
+        try:
+            return get_user_model().objects.get(**kwargs)
+        except get_user_model().DoesNotExist:
+            return GraphQLError('User does not exists')
