5b4642b2ede322015c4632e79a29e5b6703e7432
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-06 12:25:10 -0400

    feat: mostrando resource y category en el admin

--- justship/apps/resources/admin.py
+++ justship/apps/resources/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name', 'description']
+    list_display = ['name']
+    search_fields = ['name', 'description']
+
+
+@admin.register(models.Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    fields = ['url', 'category', 'image', 'description', 'creator']
+    list_display = ['url', 'description', 'creator']
+    search_fields = ['url', 'description']
--- justship/apps/resources/models.py
+++ justship/apps/resources/models.py
@@ -8,6 +8,13 @@ class Category(models.Model):
     name = models.CharField(verbose_name='Name', max_length=20)
     description = models.CharField(verbose_name='Description', max_length=200, null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
 
 class Resource(TimeStampedModel):
     url = models.URLField()
@@ -22,3 +29,8 @@ class Resource(TimeStampedModel):
     image = models.URLField(verbose_name='Image', null=True, blank=True)
     description = models.TextField(verbose_name='Description', null=True, blank=True)
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Resource'
+        verbose_name_plural = 'Resources'
+        ordering = ['created_at']
