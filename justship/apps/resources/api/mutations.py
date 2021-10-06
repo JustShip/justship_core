304292083a979c9ae0ed47dc357a95c67d6db14a
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-06 14:12:28 -0400

    feat: add category

--- justship/apps/resources/api/mutations.py
+++ justship/apps/resources/api/mutations.py
@@ -1,14 +1,33 @@
 import graphene
+from graphql import GraphQLError
+
+from justship.apps.resources import models
 
 
-class CreateResource(graphene.Mutation):
+class CreateCategory(graphene.Mutation):
     class Arguments:
-        pass
+        name = graphene.String()
+        description = graphene.String(required=False)
+
+    status = graphene.Boolean()
 
     @staticmethod
-    def mutate(root, info, url):
-        pass
+    def mutate(root, info, name, description=None):
+        """
+        Add category if doesn't exists. Only staff can add categories
+        :param root:
+        :param info:
+        :param name: category's name
+        :param description: category's description. Not required
+        :return: a boolean
+        """
+        if info.context.user.is_staff:
+            # create only if doesn't exists
+            category, is_created = models.Category.objects.get_or_create(name=name, description=description)
+            return CreateCategory(status=is_created)
+        else:
+            return GraphQLError('You must be staff')
 
 
 class ResourceMutations:
-    add_resource = CreateResource.Field()
+    add_category = CreateCategory.Field()
