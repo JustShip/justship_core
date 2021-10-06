0d3db5be51a91f236154b58d53e14db8e5eaf4b9
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-06 11:17:08 -0400

    feat: modelo Resource agregado

--- justship/apps/resources/models.py
+++ justship/apps/resources/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Resource(models.Model):
+    url = models.URLField()
+    category = models.CharField(verbose_name='Category', max_length=100)
+    image = models.URLField(verbose_name='Image', null=True, blank=True)
+    description = models.TextField(verbose_name='Description')
