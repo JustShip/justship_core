4e6fe7637214ce55dda1e908a74d6d0235f05769
Carlos Lugones <contact@lugodev.com>
2021-10-07 04:23:16 -0400

    feat: product models
    - Added Tag model
    - Added Social model
    - Added Product model
    - Improved Resource model

--- justship/apps/products/models.py
+++ justship/apps/products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from justship.apps.core.models import Tag, Social
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    link = models.URLField(null=True, blank=True)
+    state = models.CharField(max_length=255, default='pending', choices=(
+        ('building', 'Building'),
+        ('operating', 'Operating'),
+        ('closed', 'Closed')
+    ))
+
+    tags = models.ManyToManyField(Tag)
+    social = models.ManyToManyField(Social)
+
+    # logo
+    logo_file_id = models.CharField(max_length=255, null=True, blank=True)
+    logo_url = models.URLField(null=True, blank=True)
+    logo_thumbnail_url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
--- justship/apps/resources/models.py
+++ justship/apps/resources/models.py
@@ -8,26 +8,26 @@ class Category(models.Model):
     name = models.CharField(verbose_name='Name', max_length=20)
     description = models.CharField(verbose_name='Description', max_length=200, null=True, blank=True)
 
-    def __str__(self):
-        return self.name
-
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
 
+    def __str__(self):
+        return self.name
+
 
 class Resource(TimeStampedModel):
     url = models.URLField()
-    category = models.ManyToManyField(
+    image = models.URLField(verbose_name='Image', null=True, blank=True)
+    description = models.TextField(verbose_name='Description', null=True, blank=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(
         Category,
         related_name='categories',
         verbose_name='Category',
         max_length=100,
         blank=True
     )
-    image = models.URLField(verbose_name='Image', null=True, blank=True)
-    description = models.TextField(verbose_name='Description', null=True, blank=True)
-    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Resource'
