d728f475596c56fbf9b935ad9c8f2752d1b047ae
Carlos Lugones <contact@lugodev.com>
2021-10-07 04:31:43 -0400

    feat: ProductType

--- justship/apps/products/api/types.py
+++ justship/apps/products/api/types.py
@@ -1,1 +1,24 @@
-product_types = []
+from graphene_django import DjangoObjectType
+
+from ..models import Product
+
+
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+        only_fields = (
+            'id',
+            'name',
+            'description',
+            'link',
+            'state',
+            'tags',
+            'socials',
+            'logo_url',
+            'logo_thumbnail_url'
+        )
+
+
+product_types = [
+    ProductType
+]
