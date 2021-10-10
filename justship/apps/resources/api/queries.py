aa46955c6913098cb50bb7b56b2044db9dd426c5
Alex_Cuan <fav09@protonmail.com>
2021-10-10 18:49:28 -0400

    feat: Add single vote resolver

--- justship/apps/resources/api/queries.py
+++ justship/apps/resources/api/queries.py
@@ -2,7 +2,7 @@ import graphene
 
 from .types import CategoryType
 from .. import models
-from .types import ResourceType
+from .types import ResourceType, VoteType
 
 
 class ResourceQueries:
@@ -10,8 +10,12 @@ class ResourceQueries:
     categories = graphene.List(CategoryType)
     resource = graphene.Field(ResourceType, resource_id=graphene.Int())
     all_resources = graphene.List(ResourceType)
+    vote = graphene.Field(VoteType, vote_url=graphene.String())
 
-    def resolve_resource(self, info, resource_id=graphene.Int()):
+    def resolve_vote(self, info, vote_url):  # TO-DO: Change search argument or keep it
+        return models.Vote.objects.get(resource=models.Resource.objects.get(url=vote_url))
+
+    def resolve_resource(self, info, resource_id):
         return models.Resource.objects.get(pk=resource_id)
 
     def resolve_all_resources(self, info):
