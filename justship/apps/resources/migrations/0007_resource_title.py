a4e1646134972fe7508bb5b25d14cefe5e469950
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-11 14:30:09 -0400

    fix: migrations conflicts

--- justship/apps/resources/migrations/0007_resource_title.py
+++ justship/apps/resources/migrations/0007_resource_title.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='resource',
             name='title',
-            field=models.CharField(default=None, max_length=100, verbose_name='Title'),
+            field=models.CharField(default='', max_length=100, verbose_name='Title'),
             preserve_default=False,
         ),
     ]
