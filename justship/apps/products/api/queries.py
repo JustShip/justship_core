90f2eaa3ed844dba92d1e595229134f642826344
Alex_Cuan <fav09@protonmail.com>
2021-10-16 18:38:35 -0400

    feat: Add query that resolves products information

--- justship/apps/products/api/queries.py
+++ justship/apps/products/api/queries.py
@@ -1,5 +1,10 @@
 import graphene
+from .types import ProductType
+from .. import models
 
 
 class ProductQueries:
-    pass
+    product = graphene.Field(ProductType, product_id=graphene.Int())
+
+    def resolve_product(self, info, product_id):
+        return models.Product.objects.get(pk=product_id)
