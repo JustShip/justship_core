08e287e57e62074cbb865f47453acb3f7c8eec04
Alex_Cuan <fav09@protonmail.com>
2021-10-20 00:17:01 -0400

    feat: Add 'CreateTag' mutation to Core API

--- justship/apps/core/api/mutations.py
+++ justship/apps/core/api/mutations.py
@@ -1,1 +1,27 @@
 import graphene
+from graphql import GraphQLError
+from graphql_jwt.decorators import login_required
+
+from .types import TagType
+from justship.apps.core.models import Tag
+
+
+class CreateTag(graphene.Mutation):
+    tag = graphene.Field(TagType)
+
+    class Arguments():
+        name = graphene.String()
+
+    @staticmethod
+    @login_required
+    def mutate(root, info, name):
+        try:
+            tag = Tag(name=name)
+            tag.save()
+            return CreateTag(tag=tag)
+        except:
+            return GraphQLError("Tag already exists")
+
+
+class CoreMutations(graphene.ObjectType):
+    create_tag = CreateTag.Field()
